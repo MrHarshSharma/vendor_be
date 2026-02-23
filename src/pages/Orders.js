adb5e95ab5d32b103c729acaed064bed5dd458a1
Harsh Sharma <harshsharma@Harshs-MacBook-Air.local>
2026-02-23 19:08:21 +0530

    removed eslint error added append order support

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import { db } from "../firebase/setup";
 import AppLayout from "./AppLayout";
